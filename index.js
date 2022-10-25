cd847e44a066f508cc0d3202b51c9140589fcffc
Jestthecoder85 <majesticwilson85@gmail.com>
2022-10-25 17:34:20 -0400

    updated so far

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let content = document.querySelector('.js-generated.content')
 
-let = document.createElement('h1')
+let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
 header.append('Rizzo')
 content.append(header)
@@ -13,13 +13,22 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
 let header3 = document.createElement('h3')
-header3.append('Description')
+header3.append('Description:')
+dogDetails.append(header3)
 
- 
+let pTag = document.createElement('p')
+pTag.append(`This gentle dog is aloof toward her owner, and never comes when called.
+She always acts as though any stranger she meets will harm her, and
+dislikes other animals.`)
+dogDetails.append(pTag)
+
+let header3 = document.createElement('h3')
+header3.append('Feeding Times:')
+dogDetails.append(header3)
